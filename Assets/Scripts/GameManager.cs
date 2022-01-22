1ce26c1445b0eb34c3029f5a03474f797c1423e6
Sebastian Yang <47506432+SCP650@users.noreply.github.com>
2022-01-22 13:18:10 -0500

    reset works

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager i { get; private set; }
+    public GameObject[] rooms;
+    public int currentRoom = -1;
+    private GameObject currRoomInstantce;
+
+    private void Awake()
+    {
+        if (i != null && i != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            i = this;
+        }
+    }
+
+    private void Start()
+    {
+        SpawnRoom(i.currentRoom);
+    }
+
+    private void SpawnRoom(int RoomNum)
+    {
+        currRoomInstantce = Instantiate(rooms[RoomNum]);
+        currRoomInstantce.transform.position = Vector3.zero;
+    }
+
+    public void ResetRoom()
+    {
+        Destroy(currRoomInstantce);
+        SpawnRoom(currentRoom);
+    }
+
+    public void SpawnNextRoom()
+    {
+        currentRoom += 1;
+        currentRoom %= rooms.Length;
+        SpawnRoom(currentRoom);
+    }
+
+
+}
